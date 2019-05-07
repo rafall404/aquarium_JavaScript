849c9ee741db588a7b67ff7995c3a5ab8b9beebd
Rafal Pierscieniak <279471@via.dk>
2019-05-07 19:38:29 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,41 +1,43 @@
 $(document).ready(function(){
 
-  animateDiv("#fish1Id");
-  animateDiv("#fish2Id");
-  animateDiv("#divingman");
-  animateTurtle("#turtle");
-  animateBubbles("#bubble1Id");
-  animateBubbles("#bubble2Id");
-  animateBubbles("#bubble3Id");
+  //randomPosition
+  function randomPosition(){
+
+    var h = $(window).height() - 20;
+    var w = $(window).width() - 20;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh,nw];
+  }
 
 
   $("#fish2Id").on("mouseenter", function(){
     $("#fish2Id").stop(true);
-    var randomHeight = Math.max(0, Math.floor(Math.random() * $(window).height()) - $(this).height());
-    var randomWidth = Math.max(0, Math.floor(Math.random() * $(window).width()) - $(this).width());
+    [randomHeight, randomWidth] = randomPosition();
     $('#fish2Id').animate({top: randomHeight, left: randomWidth});
-    animateDiv("#fish2Id");
+    animateDiv("#fish2Id", 5000);
   });
 
   $(document).click(function (event) {
     $("#fish1Id").stop(true);
     $("#fish1Id").animate({left: event.pageX - 100, top: event.pageY - 100}, 700);
-    animateDiv("#fish1Id");
+    animateDiv("#fish1Id", 5000);
   });
 
 
     $("#fish1Id").on('dblclick', function(){
-      console.log('suka');
-      $("#fish1Id").css({"height": "500px","width": "500px"})
-        settimeout(function(){
-          $("#fish1Id").css({"height": "250px", "width": "250px"}, )
-        }, 3000)
-        animateDiv("#fish1Id");
-
+      $(this).stop(true);
+      $("#fish1Id").animate({"height": "500px","width": "500px"})
+        .delay(2000)
+        .animate({"height": "250px", "width": "250px"}, function() {
+            animateDiv("#fish1Id", 5000);
+          });
     });
 
 //randomStartPossition
-    function getRandomStartPosBottom(itemId){
+function getRandomStartPosBottom(itemId){
     var offsetW = $(itemId).innerWidth();
     var w = $(window).width() - offsetW;
     var nw = Math.floor(Math.random() * w);
@@ -43,20 +45,20 @@ $(document).ready(function(){
 }
 
 //bubbleAnimation
-    var bubbleAnimationTime = 13000;
-function animateBubbles(itemId){
+// var bubbleAnimationTime = 13000;
+function animateBubbles(itemId, time){
     var sw = getRandomStartPosBottom(itemId);
     var sh = $(window).height();
     $(itemId).offset({top: sh, left: sw})
-    $(itemId).animate({ top: -100}, bubbleAnimationTime,   function(){
-        animateBubbles(itemId);
+    $(itemId).animate({ top: -100}, time, function(){
+        animateBubbles(itemId, time);
     }).click(function(event){
         event.stopPropagation()
         $(itemId).stop();
         $(itemId).fadeOut(400, function(){
             $(itemId).stop(true);
             $(itemId).fadeIn(1);
-            animateBubbles(itemId);
+            animateBubbles(itemId, time);
         });
     });
 }
@@ -66,52 +68,46 @@ function animateBubbles(itemId){
 
 var divingman = '<img id="divingman" src="images/divingman.png" />';
 $('body').append(divingman);
+$("#divingman").css({"height": "300px", "width": "300px"})
 
-
+var animatingDiver = false;
 $(document).keydown(function(e){
+  if(animatingDiver)
+    return;
+  animatingDiver = true;
   console.log(e.which);
   switch (e.which){
     case 37:
     $("#divingman").css({"transform": "rotate(0deg)"})
-      $("#divingman").animate({left: "-=100px"});
+      $("#divingman").animate({left: "-=100px"}, 400, 'linear', () => {animatingDiver = false});
       break;
     case 39:
     $("#divingman").css({"-webkit-transform": "scaleY(-1)", "transform": "scaleX(-1)"})
-      $('#divingman').animate({left: "+=100px"});
+      $('#divingman').animate({left: "+=100px"}, 400, 'linear', () => {animatingDiver = false});
       break;
     case 38:
     $("#divingman").css({"transform": "rotate(90deg)"})
-    $('#divingman').animate({top: "-=100px"});
+    $('#divingman').animate({top: "-=100px"}, 400, 'linear', () => {animatingDiver = false});
       break;
     case 40:
     $("#divingman").css({"transform": "rotate(-90deg)"})
-    $('#divingman').animate({top: "+=100px"});
+    $('#divingman').animate({top: "+=100px"}, 400, 'linear', () => {animatingDiver = false});
       break;
   }
 })
 
-// CSS
-
-$("#divingman").css({"height": "300px", "width": "300px"})
-$("#turtle").css({"height": "300px", "width": "300px"})
-
-//randomPositon
-function randomPosition(){
-
-  var h = $(window).height() - 20;
-  var w = $(window).width() - 20;
-
-  var nh = Math.floor(Math.random() * h);
-  var nw = Math.floor(Math.random() * w);
-
-  return [nh,nw];
-}
 
 //randomAnimateFish
-function animateDiv(myClass){
+function animateDiv(id, speed){
   var newq = randomPosition();
-  $(myClass).animate({ top: newq[0], left: newq[1] }, 2000,   function(){
-    animateDiv(myClass);
+
+  var dir = $(id).offset().left < newq[1] ? 1 : -1;
+  $(id).css({transform: `scaleX(${dir})`})
+      .animate({
+          top: newq[0],
+          left: newq[1],
+        }, speed, function() {
+    animateDiv(id, speed);
   });
 }
 
@@ -119,13 +115,14 @@ function animateDiv(myClass){
 
 var turtle = '<img id="turtle" src="images/turtle.png" />';
 $('body').append(turtle);
+$("#turtle").css({"height": "300px", "width": "300px"})
 
-function animateTurtle(myClass2){
-  var newq = randomPosition();
-  $(myClass2).animate({ top: newq[0], left: newq[1] }, 5000,   function(){
-    animateTurtle(myClass2);
-  });
-}
 
+  animateDiv("#fish1Id", 2000);
+  animateDiv("#fish2Id", 2000);
+  animateDiv("#turtle", 5000);
+  animateBubbles("#bubble1Id", 2000);
+  animateBubbles("#bubble2Id", 5000);
+  animateBubbles("#bubble3Id", 3000);
 
 })
